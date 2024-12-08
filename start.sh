9a7d58c1079cc2a163dd2220ee247be3b2c09413
Jaks <jcqs.gr@gmail.com>
2024-12-08 16:35:40 +0100

    readme

--- start.sh
+++ start.sh
@@ -2,7 +2,12 @@
 
 composer install
 
-docker exec -it php-service php bin/console c:c
-docker exec -it php-service php bin/console d:m:m
+docker compose build
+docker compose up -d
+
+docker exec -it php php bin/console c:c
+docker exec -it php php bin/console d:m:m
+
+echo " ✔ App running at http://localhost:8000"
 
 docker compose watch
